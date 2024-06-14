d27dcaf4f41311f830a4ba5e167cf34899bf3e84
Mirsoip <mirsoip.adilov@gmail.com>
2024-06-14 15:24:40 +0500

    Second Commit. Authentification part is done. Video stopped at 42:20

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 import { Loader2 } from "lucide-react";
-import { SignIn, ClerkLoading, ClerkLoaded } from "@clerk/nextjs";
+import { SignUp, ClerkLoading, ClerkLoaded } from "@clerk/nextjs";
 
 export default function Page() {
   return (
@@ -14,7 +14,7 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center mt-8">
           <ClerkLoaded>
-            <SignIn />
+            <SignUp />
           </ClerkLoaded>
           <ClerkLoading>
             <Loader2 className="animate-spin text-muted-foreground" />
